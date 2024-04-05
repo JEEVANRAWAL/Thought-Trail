7255c0acc7cb73746dc968894dbd5598dac23ccc
Jeevan Rawal <rawaljeevan123@gmail.com>
2024-04-05 18:43:16 +0545

    state management pipia used to maintain client state

--- blogging_frontend/src/stores/BlogStorage.js
+++ blogging_frontend/src/stores/BlogStorage.js
@@ -2,12 +2,23 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const blogsStorage= defineStore('blogStore',()=>{
-    const blog= ref([]);
+    const rowsPerPage= ref(null);
+    const page= ref(1);
 
-    function appendBlog(data){
-        // console.log(data);
-        blog.value.push(data);
+
+    const blogFetcher= async (page, rowsPerPage)=>{
+        try{
+            const response= await fetch(`http://127.0.0.1:8000/api/fetchBlogs?page=${page}&rowsPerPage=${rowsPerPage}`);
+            const data = await response.json();
+            return data;
+        }catch(error){
+            console.error("error occures during fetching Blogs", error);
+        }
+    };
+
+    function action(data){
+        page.value= page.value + 1;
     }
 
-    return{ blog, appendBlog}
+    return{ rowsPerPage, page, blogFetcher}
 })
