5167f8a6c75779de5229dd12a3c648944b8e4ac8
Jeevan Rawal <rawaljeevan123@gmail.com>
2024-04-06 17:52:05 +0545

    blogs seeder created and new data seeded in database

--- blogging_backend/blogging_backend/database/factories/BlogFactory.php
+++ blogging_backend/blogging_backend/database/factories/BlogFactory.php
@@ -2,7 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\Blog;
 use Illuminate\Database\Eloquent\Factories\Factory;
+// use Unsplash\Photo;
+// use Unsplash\Search\PhotoSearch;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
@@ -14,10 +18,24 @@ class BlogFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+     protected $model = Blog::class;
+
     public function definition(): array
     {
+        $paragraphs = $this->faker->paragraphs(3, true); // Generate 3 paragraphs of text
+        // $photoSearch = new PhotoSearch();
+        // $photos = $photoSearch->getRandomPhotos(['query' => 'nature', 'count' => 1]);
+        // $imageUrl = $photos[0]->getUrls()->getRegular();
+
         return [
-            //
+            'user_id' => 1,
+            'title' => $this->faker->sentence,
+            'post' => '<p>' . $paragraphs . '</p>',
+            'slug' => $this->faker->slug,
+            'featuredImage' => $this->faker->imageUrl(600, 300, 'nature'),
+            'metaDescription' => $this->faker->sentence,
+            'post_excerpt' => $this->faker->paragraph,
         ];
     }
 }
