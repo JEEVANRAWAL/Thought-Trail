c0b1e4e344dbf6e8a342b1644610fb962d1102fd
Jeevan Rawal <rawaljeevan123@gmail.com>
2024-04-11 01:37:14 +0545

    minor changes

---
+++ blogging_frontend/src/components/DialogComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="dialogWrapper">
+    
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- blogging_frontend/src/components/Editor-WYSIWYG-Component.vue
+++ blogging_frontend/src/components/Editor-WYSIWYG-Component.vue
@@ -1,4 +1,5 @@
 <template>
+  
       <q-editor id="blogEditor"
         ref="editor"
         flat
@@ -136,36 +137,17 @@
 
 
       </q-editor>
-      <!-- <q-dialog v-model="unsplash" transition-show="rotate" transition-hide="rotate">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">Terms of Agreement</div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <p v-for="n in 15" :key="n">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</p>
-        </q-card-section>
-
-        <q-card-actions align="right">
-          <q-btn flat label="Decline" color="primary" v-close-popup />
-          <q-btn flat label="Accept" color="primary" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog> -->
- 
 </template>
 
 <script setup>
 import { ref, defineEmits } from "vue";
 import axios from 'axios';
 
-
 const editor=ref();
 const image=ref();
 const imageUrl = ref('');
 const imgWidth=ref('');
 const imgHeight= ref('');
-const unsplash= ref(false);
 
 const emit= defineEmits(['ImageUrl'])
 
