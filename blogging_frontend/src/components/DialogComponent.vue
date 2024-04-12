306c46f4d2c6def96163ecca4ddfb46c70ca053b
Jeevan Rawal <rawaljeevan123@gmail.com>
2024-04-12 16:01:59 +0545

    login dialog box added

--- blogging_frontend/src/components/DialogComponent.vue
+++ blogging_frontend/src/components/DialogComponent.vue
@@ -1,10 +1,47 @@
 <template>
-  <div class="dialogWrapper">
-    
-  </div>
+    <q-dialog :backdrop-filter="backdropFilter">
+      <q-card class="dialog-box">
+        <q-card-section class="row items-center q-pb-none text-h6">
+          Login Form
+          <q-btn style="margin-left: 16vw;" flat label="Close" color="primary" v-close-popup />
+        </q-card-section>
+
+        <q-card-section>
+          <q-input class="dialog-box-input" outlined v-model="loginData.email" label="email" :dense="dense" />
+        </q-card-section>
+        <q-card-section>
+          <q-input class="dialog-box-input" outlined v-model="loginData.password" label="password" type="password" :dense="dense" />
+        </q-card-section>
+
+        <q-card-section>
+          <q-btn color="green"  label="Login" @click="login(loginData)"/>
+        </q-card-section>
+
+      </q-card>
+    </q-dialog>
 </template>
 
-<script>
+<script setup>
+import { reactive } from 'vue';
+import axios from 'axios';
+
+
+const loginData=reactive({
+  email:'',
+  password:''
+})
+
+async function login(data){
+  const jsonformatedData= JSON.stringify(data)
+  const config= {headers:{'Content-Type': 'application/json'}}
+  try{
+    // console.log(jsonformatedData);
+    const response= await axios.post('http://127.0.0.1:8000/api/login', jsonformatedData, config);
+    console.log(response.data);
+  }catch(error){
+
+  }
+}
 
 </script>
 
